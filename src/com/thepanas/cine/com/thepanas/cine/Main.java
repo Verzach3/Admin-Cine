cbde34b2e46b99ba6b81e41983ffbd31dcf45c7c
Verzach3 <gabrielcorreacardenas@gmail.com>
2021-01-25 16:00:10 -0500

    First Commit
    Añadidos los primeros archivos

---
+++ src/com/thepanas/cine/com/thepanas/cine/Main.java
@@ -0,0 +1,18 @@
+package com.thepanas.cine;
+
+import javax.swing.JFrame;
+import java.awt.Graphics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola pro");
+        
+        new seatViewer();
+
+
+
+
+    }
+}
