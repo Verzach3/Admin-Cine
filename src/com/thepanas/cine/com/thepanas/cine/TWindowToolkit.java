6b505f89b69b780147b7d943ef13d1dd3ee19af9
Verzach3 <gabrielcorreacardenas@gmail.com>
2021-01-25 16:00:10 -0500

    First Commit
    Algunos Cambios

---
+++ src/com/thepanas/cine/com/thepanas/cine/TWindowToolkit.java
@@ -0,0 +1,21 @@
+package com.thepanas.cine;
+
+import java.awt.Graphics;
+
+
+public class TWindowToolkit {
+
+    void TInputBox() {
+
+    }
+
+    void TButton(int buttonX, int buttonY, int buttonWidth, int buttonHeight) {
+
+    }
+
+    public void paint(Graphics g){
+        
+        
+
+    }
+}
---
+++ src/com/thepanas/cine/com/thepanas/cine/seatViewer.java
@@ -0,0 +1,36 @@
+package com.thepanas.cine;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class seatViewer extends JFrame {
+
+    seatViewer( ){
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+
+        int sillas2D = 80;
+        int sillas3D = 50;
+        int sillasVip = 10;
+        
+
+        boolean arraySillas2D [] = new boolean[sillas2D];
+
+        boolean arraySillas3D [] = new boolean[sillas3D];
+
+        boolean arraySillasVip [] = new boolean[sillasVip];  
+        
+
+    }
+
+    public void paint(Graphics g){
+        
+        g.draw3DRect(0, 0, 100, 100, false);
+
+    }
+
+}
