32e58688d2b5139fac47d2b217647e9b57ecaf0f
Verzach3 <gabrielcorreacardenas@gmail.com>
2021-01-27 01:29:15 -0500

    CAMBIOS
    toda la gui aparte del codigo principal

---
+++ src/com/thepanas/cineadmin/seatManager.java
@@ -0,0 +1,22 @@
+package com.thepanas.cineadmin;
+
+public class seatManager {
+
+    seatManager( ){
+
+
+        int sillas2D = 80;
+        int sillas3D = 50;
+        int sillasVip = 10;
+
+
+        boolean arraySillas2D [] = new boolean[sillas2D];
+
+        boolean arraySillas3D [] = new boolean[sillas3D];
+
+        boolean arraySillasVip [] = new boolean[sillasVip];
+
+    }
+
+
+}
