984bfa77b31aeb7791b1081fbef7f359181d5642
Kahyberth Stiven Gonzalez Sayas <kahyberthst@gmail.com>
2021-02-11 11:16:25 -0500

    Cambio
    Print

--- src/com/thepanas/CinemaFilms/Main.java
+++ src/com/thepanas/CinemaFilms/Main.java
@@ -3,6 +3,6 @@ package com.thepanas.CinemaFilms;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola primo");
+        System.out.println("Cambio");
     }
 }
