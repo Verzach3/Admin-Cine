b2a49b9946438c7adb44819e2811c9695ad82643
Verzach3 <gabrielcorreacardenas@gmail.com>
2021-02-11 19:37:20 -0500

    Cambios
    Minor changes

--- src/main/java/billGenerator.java
+++ src/main/java/billGenerator.java
@@ -1,19 +1,36 @@
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 
 import java.io.*;
 
 public class billGenerator {
+    
+
+
+    
+    // Formato de la matriz para las faturas : Pelicula, Tipo de funcion (2D o 3D), Horario, Sala, Clasificacion, Sillas
+    
+    //Matriz de prueba
+    String[] matrizDemoInvoice = {"Jumanji", "2D", "9pm a 11pm", "2", "+12", "A12, A13"};
+    //Ruta de destino del pdf
     public static final String DEST = "/home/verzach3/hello.pdf";
 
-    public static void generarPDF() throws IOException {
+    //Funcion principal para general la factura
+    public static void generateBill() throws IOException {
+ 
         PdfDocument pdf = new PdfDocument(new PdfWriter(DEST));
         Document document = new Document(pdf);
+        document.setTextAlignment(TextAlignment.CENTER);
+        document.setFontSize(40);
         String line = "Hello! Welcome to iTextPdf";
         document.add(new Paragraph(line));
+
         document.close();
 
-        System.out.println("Awesome PDF just got created.");
+        //Mensaje de confirmacion del pdf
+        System.out.println("Factura creada correctamente");
     }
 }
