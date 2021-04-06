4ff17dad8c78bad3ed382b204c61f63877c6ba2f
Kahyberth Gonzalez <kahyberth.gonzalez@correounivalle.edu.co>
2021-04-05 22:04:29 -0500

    TBarGraphics V1

---
+++ src/main/java/com/thepanas/GUILib/TBarGraphics.java
@@ -0,0 +1,90 @@
+package com.thepanas.GUILib;
+//------------------->Importaciones
+import javax.swing.*;
+import java.awt.*;
+/*
+TBarGraphics ---------------> V1
+ */
+
+//---------------->TBarGraphics
+public class TBarGraphics extends JPanel {
+    private double[] values;
+
+    private String title;
+
+    public TBarGraphics(double[] v, String t) {
+
+        values = v;
+        title = t;
+    }
+
+    //-------------------->PaintComponent
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (values == null || values.length == 0)
+            return;
+        double minValue = 0;
+        double maxValue = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (minValue > values[i])
+                minValue = values[i];
+            if (maxValue < values[i])
+                maxValue = values[i];
+        }
+
+        //-------------------->Dimensiones
+        Dimension d = getSize();
+        int clientWidth = d.width;
+        int clientHeight = d.height;
+        int barWidth = clientWidth / values.length;
+
+        //---------------->Cambio de Fuente
+        Font titleFont = new Font("Arial", Font.BOLD, 20);
+        FontMetrics titleFontMetrics = g.getFontMetrics(titleFont);
+        Font labelFont = new Font("Arial", Font.PLAIN, 10);
+        FontMetrics labelFontMetrics = g.getFontMetrics(labelFont);
+
+        //----------------->Titulo
+        int titleWidth = titleFontMetrics.stringWidth(title);
+        int y = titleFontMetrics.getAscent();
+        int x = (clientWidth - titleWidth) / 2;
+        g.setFont(titleFont);
+        g.drawString(title, x, y);
+        int top = titleFontMetrics.getHeight();
+        int bottom = labelFontMetrics.getHeight();
+        if (maxValue == minValue)
+            return;
+        double scale = (clientHeight - top - bottom) / (maxValue - minValue);
+        y = clientHeight - labelFontMetrics.getDescent();
+        g.setFont(labelFont);
+        int contador = 0; //---------------------------------->Contador definido
+        //-------------------->Bucle
+        for (int i = 0; i < values.length; i++) {
+            int valueX = i * barWidth + 1;
+            int valueY = top;
+            int height = (int) (values[i] * scale);
+            if (values[i] >= 0)
+                valueY += (int) ((maxValue - values[i]) * scale);
+            else {
+                valueY += (int) (maxValue * scale);
+                height = -height;
+            }
+    //-------------------------------------------------->
+    //------------------------------------------------->
+            g.setColor(Color.BLUE);//--------------------------->Color de las barras (Grafica)
+            g.fillRect(valueX, valueY, barWidth - 2, height);//------------------------>Barras
+            g.setColor(Color.BLACK);//--------------------------->Color del contorno y letras de dato
+            g.drawRect(valueX, valueY, barWidth - 2, height);
+            int labelWidth = labelFontMetrics.stringWidth("Datos: "+i); //--------------->Datos muestra los datos ++
+            x = i * barWidth + (barWidth - labelWidth) / 2;
+            contador++;//----------------------------->Contador de datos
+            g.drawString("Datos: "+contador, x, y);//-------------------------------->Datos Ubicacion
+
+            g.drawString("Valor: "+values[i],valueX,valueY);//--------------->Muestra los valores asignados
+
+            //------------------------->Personalizar Barras Color*
+
+
+        }
+    }
+}
---
+++ src/main/java/com/thepanas/GUILib/TWindowsTest.java
@@ -0,0 +1,45 @@
+package com.thepanas.GUILib;
+//Librerias
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+/*
+Prueba del TBarGraphics Recordar que el TBarGraphics recibe un Array
+ y lo grafica con los valores asignados o guardados
+ */
+public class TWindowsTest{
+    public static void main(String[] argv) {
+        JFrame f = new JFrame();
+        f.setSize(400, 300); //---------------->Tamaño definido de la ventana de prueba
+        double[] values = new double[5]; //--------------------> Bucle con valores que quiero graficar
+
+        //----------------------->Valores
+        values[0] = 9;
+
+        values[1] = 7;
+
+        values[2] = 4;
+
+        values[3] = 5;
+
+        values[4] = 2;
+        //---------------------------------------------------->
+
+        //---->ContentPane y ---> se ingresa -->Array ----->Titulo
+        f.getContentPane().add(new TBarGraphics(values, "TBarGraphics"));
+
+        //--->WindowsListener
+        WindowListener wndCloser = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        f.addWindowListener(wndCloser);
+        f.setVisible(true);
+    }
+}
+
+
+
+
